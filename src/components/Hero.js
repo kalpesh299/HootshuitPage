7135f9f323b5f78dcc550cda9224034a1f059113
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-03 13:12:10 +0530

    .

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className="bg-blue-950 h-80 flex flex-col justify-center items-center">
+ <div class="mx-auto max-w-[43rem] ">
+    <div class="text-center">
+
+      <h1 class="mt-3 text-[3rem] font-black leading-[4rem] tracking-tight text-white ">See Value From Social Sooner</h1>
+      <p class="mt-3 text-lg leading-relaxed text-white">Our Training and strategic services have helped thousands of organizations succeed on social.</p>
+    </div>
+
+    <div class="mt-6 flex items-center justify-center gap-4">
+
+      <a href="#" class="bg-green-400 transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> Request a demo </a>
+    </div>
+  </div>
+    </div>
+  )
+}
